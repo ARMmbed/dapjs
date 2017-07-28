3f74eb3277d902d7303bf44742a35ecc08a3dfba
Matthew Else <matthewelse1997@gmail.com>
2017-07-28 09:38:59 +0100

    testing out uploading analyzer with flashing algorithm

--- src/targets/MbedTarget.ts
+++ src/targets/MbedTarget.ts
@@ -24,6 +24,16 @@ export interface IFlashAlgo {
     flashStart: number;
 }
 
+const analyzer = new Uint32Array([
+    0x2180468c, 0x2600b5f0, 0x4f2c2501, 0x447f4c2c, 0x1c2b0049, 0x425b4033, 0x40230872, 0x085a4053,
+    0x425b402b, 0x40534023, 0x402b085a, 0x4023425b, 0x085a4053, 0x425b402b, 0x40534023, 0x402b085a,
+    0x4023425b, 0x085a4053, 0x425b402b, 0x40534023, 0x402b085a, 0x4023425b, 0x085a4053, 0x425b402b,
+    0x40534023, 0xc7083601, 0xd1d2428e, 0x2b004663, 0x4663d01f, 0x46b4009e, 0x24ff2701, 0x44844d11,
+    0x1c3a447d, 0x88418803, 0x4351409a, 0xd0122a00, 0x22011856, 0x780b4252, 0x40533101, 0x009b4023,
+    0x0a12595b, 0x42b1405a, 0x43d2d1f5, 0x4560c004, 0x2000d1e7, 0x2200bdf0, 0x46c0e7f8, 0x000000b6,
+    0xedb88320, 0x00000044,
+]);
+
 export class MbedTarget extends FlashTarget {
     private flashAlgo: IFlashAlgo;
 
@@ -43,6 +53,8 @@ export class MbedTarget extends FlashTarget {
         await this.halt();
 
         await this.writeCoreRegister(CortexReg.R9, this.flashAlgo.staticBase);
+        console.log("Uploading anaylyzer");
+        await this.memory.writeBlock(0x1ffff000, analyzer);
 
         const result = await this.runCode(
             this.flashAlgo.instructions,
@@ -113,23 +125,8 @@ export class MbedTarget extends FlashTarget {
             console.log("Flashed first block.");
         }
     }
-
-    private async resetStopOnReset() {
-        console.log("reset stop on Reset");
-
-        await this.halt();
-
-        const demcr = await this.memory.read32(CortexSpecialReg.DEMCR);
-
-        await this.memory.write32(CortexSpecialReg.DEMCR, demcr | CortexSpecialReg.DEMCR_VC_CORERESET);
-        await this.reset();
-
-        await this.waitForHalt();
-
-        await this.memory.write32(CortexSpecialReg.DEMCR, demcr);
-    }
 }
 
 export let FlashAlgos = new Map<string, IFlashAlgo>();
-FlashAlgos.set('0240', K64F_FLASH_ALGO);
-FlashAlgos.set('9900', MICROBIT_FLASH_ALGO);
+FlashAlgos.set("0240", K64F_FLASH_ALGO);
+FlashAlgos.set("9900", MICROBIT_FLASH_ALGO);
