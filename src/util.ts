5f7df8089a3c8be651a6fbc9e75fd7eb3e618584
arekzaluski <arkadiusz.zaluski@arm.com>
2018-02-21 20:10:12 +0000

    Remove unused hex2bin method

--- src/util.ts
+++ src/util.ts
@@ -113,13 +113,3 @@ export const hexBytes = (bytes: number[]) => {
 
     return r.toUpperCase();
 };
-
-export const hex2bin = (hexstr: string) => {
-    const array = new Uint8Array(hexstr.length / 2);
-
-    for (let i = 0; i < hexstr.length / 2; i++) {
-        array[i] = parseInt(hexstr.substr(2 * i, 2), 16);
-    }
-
-    return array;
-};
