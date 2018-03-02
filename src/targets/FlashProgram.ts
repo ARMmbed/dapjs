e169ba50622db078ce97a0de8936197ece4bf938
arekzaluski <arkadiusz.zaluski@arm.com>
2018-03-02 13:58:39 +0000

    Move isBufferBinary to Utils.

--- src/targets/FlashProgram.ts
+++ src/targets/FlashProgram.ts
@@ -1,4 +1,5 @@
 import * as MemoryMap from "nrf-intel-hex";
+import {Utils} from "../dap/utils";
 
 export class FlashSection {
     constructor(public address: number, public data: Uint32Array) {
@@ -39,17 +40,10 @@ export class FlashProgram {
     constructor(public sections: FlashSection[]) {}
 
     public static fromArrayBuffer(buffer: ArrayBuffer): FlashProgram {
-        // detect if buffer contains text or binary data
-        const lengthToCheck = buffer.byteLength > 50 ? 50 : buffer.byteLength;
-        const bufferString = Buffer.from(buffer).toString("utf8");
-        for (let i = 0; i < lengthToCheck; i++) {
-            const charCode = bufferString.charCodeAt(i);
-            // 65533 is a code for unknown character
-            // 0-8 are codes for control characters
-            if (charCode === 65533 || charCode <= 8) {
-                return FlashProgram.fromBinary(0, new Uint32Array(buffer));
-            }
+        if (Utils.isBufferBinary(buffer)) {
+            return FlashProgram.fromBinary(0, new Uint32Array(buffer));
         }
+        const bufferString = Buffer.from(buffer).toString("utf8");
         return FlashProgram.fromIntelHex(bufferString);
     }
 
