559ee032c180a449df7263080f939b4dcb6661dd
Rob Moran <rob.moran@arm.com>
2018-06-24 19:23:15 +0100

    Drop current structure

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
-export {CortexM} from "./cortex/cortex";
-export {CortexReg, CortexSpecialReg, CoreState, CoreNames, ISANames} from "./cortex/constants";
-export {DAP} from "./dap/dap";
-export {Serial} from "./serial/serial";
+/*
+* DAPjs
+* Copyright Arm Limited 2018
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
 
-export {FlashTarget} from "./targets/FlashTarget";
-export {FlashProgram} from "./targets/FlashProgram";
-export {FlashAlgorithm} from "./targets/FlashAlgorithm";
-export {PlatformSelector} from "./targets/PlatformSelector";
-export {HID} from "./transport/hid";
+export { HID, USB, WebUSB } from "./transport";
+export { CmsisDap } from "./cmsis-dap";
+export { Daplink } from "./daplink";
